d91be16de99bf82d8c13aa51b7bcfc5632a7d2e6
tato1981 <duber22915@gmail.com>
2023-06-17 23:26:23 -0500

    correcciones para el deployments

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ import Sequelize from 'sequelize'
 import dotenv from 'dotenv'
 dotenv.config({path: '.env'})
 
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASSWORD,{
+const db = new Sequelize(process.env.BD_DATABASE, process.env.BD_USER, process.env.BD_PASSWORD,{
      host: process.env.BD_HOST,
-     port: 3306,
+     port: process.env.DB_PORT,
      dialect: 'mysql',
      define:{
         timestamps: true, //cuendo un usuario se registra agrega dos columnas por defecto 
